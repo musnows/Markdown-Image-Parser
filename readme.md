27453014cfe7a34e860e3c227adb9e8599354a33
musnow <ezplayingd@126.com>
2023-11-10 19:33:59 +0800

    feat: 更新脚本，支持修改原有md文件，更新readme

--- readme.md
+++ readme.md
@@ -4,29 +4,59 @@
 
 原作者的博客：[如何导出简书中的全部文章（包括图片）？](https://zhuanlan.zhihu.com/p/121155268)
 
-### 运行
+## 配置项
 
-在当前路径创建 `files` 文件夹，并在 `files` 文件夹下创建 `img` 目录；当前仓库已创建[占位目录](./files/)
+在 [spider.py](./spider.py) 文件开头，提供了几个全局变量和函数作为配置项。你可以参考注释，根据你的情况对这些变量和函数进行一定的修改。
+
+```python
+# 以下你可以根据自己的需要进行修改
+IS_REPLACE_LINK = True
+"""是否修改md文件中的链接？"""
+DEFUALT_IMG_TYPE = '.png'
+"""
+给`无文件后缀`的图片url新增文件扩展名；
+`无文件后缀图片`指的是有些URL中是不包含图片的文件扩展名的。
+建议测试一下到底是什么类型的图片，一般都是png
+"""
+
+def img_url_check(img_url:str):
+    """
+    你可以修改这个函数，比如只下载某一部分url中的图片。
+    返回值为True代表需要下载当前图片
+    """
+    return True
+```
+
+## 运行
+
+在当前路径创建 `files` 文件夹，并在 `files` 文件夹下创建 `img` 目录；当前仓库已创建[占位目录](./files/)；
 
 将md文件放入 `files` 文件夹（可以包含子文件夹，但是子文件夹名不能为 `img`）
 
-> 执行之前，请务必备份原有md文件！一定要留备份！一定要留备份！
+> 执行之前，**请务必备份原有md文件**！一定要留备份！一定要留备份！
 
 安装 `python3.9` 以上版本 ，执行如下命令
 
 ```bash
 # 安装依赖项
 pip install -r requirements.txt
+# 如果国内安装较慢，尝试使用清华镜像源
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
 
-# 执行程序
+安装完毕依赖项后，即可执行程序；再次提醒，**一定要留md文件备份**！
+
+```
 python spider.py
 ```
 
 请保持网络稳定，耐心等待程序执行完成；
 
-执行完毕后，程序会生成以 `当前时间+err.json` 命名的错误图片文件，内部包含异常的图片链接。
+执行完毕后，程序会生成以 `当前时间+err.json` 命名的错误图片文件，内部包含异常的图片链接，诸如无法下载、图片url无法解析等等问题。
 
-### 图片保存说明
+如有执行异常，程序会中止。如果需要帮助，请截图报错的详细内容，并开启一个issue与我联系。
+
+### 图片保存路径说明
 
 > 如果md文件中的图片有重名，无法正常保存，会发生冲突
 
@@ -38,13 +68,15 @@ https://example.com/23/01/01/234aa23.jpg
 
 保存图片的时候，会以 `234aa23.jpg` 命名图片，保存到 [./files/img](./files/img/) 中；
 
-脚本执行结束后，您只需要使用 `notepad++`、`vscode` 等可以批量替换文本的编辑器，将原有md文件中的 `https://example.com/23/01/01/` 部分替换为本地路径，即可看到图片。
+脚本执行结束后，您只需要使用 `vscode` 等可以批量替换文本的编辑器，将原有md文件中的 `https://example.com/23/01/01/` 部分替换为本地路径，即可看到图片。
 
-个人使用本项目，主要是迁移、备份图床里面的图片（同时还能规避掉那些没有被md文件引用的图片）
+* 新版本中新增了自动修改md文件的功能，但为了统一修改，使用的是图片的文件绝对路径。（仍待功能更新）
 
-所以**默认**您的图片是已经采用过诸如 **hash、md5、uuid、时间戳** 等各类保证文件名不重复的命名方式。
+个人使用本项目，主要是迁移、备份图床里面的图片（同时还能规避掉那些没有被md文件引用的图片）当初在设立图床之时，图片的命名方式就已经使其不可能会有冲突了。
 
-如果你的md文件中的图片外链，采用的是图片原始名字作为外链名字，那么本项目并不适合您使用。因为保存图片的时候，会因为最终图片文件名相同，而导致大量冲突。
+所以**默认**您的图片url是已经采用过诸如 **hash、md5、uuid、时间戳** 等各类保证文件名不重复的命名方式。
+
+如果你的md文件中的图片外链，采用的是图片原始名字作为外链名字，那么本项目并不适合您使用。**因为保存图片的时候，会因为最终图片文件名相同，而导致大量冲突**。
 
 ----
 
@@ -61,9 +93,15 @@ https://example.com/23/01/01/234aa23.jpg
 ### 下载图片
 很简单，就是requests，没啥好说的。
 
+```python
+url = "图片网络url"
+img_data = requests.get(url).content # 图片文件数据
+```
+
 ## 实现
 ### 遍历文件
 首先要遍历文件夹里面的所有md文档：
+
 ```python
 def get_files_list(dir):
     """
@@ -81,6 +119,7 @@ def get_files_list(dir):
 
 ### 解析md文档 获取所有图片
 先用misaka把markdown转换成html，然后再拿出所有img。
+
 ```python
 def get_pics_list(md_content):
     """
@@ -100,16 +139,33 @@ def get_pics_list(md_content):
 
 ### 下载图片
 ```python
-def download_pics(url, file):
-    img_data = requests.get(url).content
-    filename = os.path.basename(file)
-    dirname = os.path.dirname(file)
-    targer_dir = os.path.join(dirname, f'{filename}.assets')
-    if not os.path.exists(targer_dir):
-        os.mkdir(targer_dir)
+def download_pics(url:str):
+    """
+    下载图片并保存到本地
 
-    with open(os.path.join(targer_dir, f'{uuid.uuid4().hex}.jpg'), 'w+') as f:
+    Returns:
+    - 空字符串：非网络图片，处理失败
+    - 非空字符串：图片本地文件名
+    """
+    if 'http' not in url:
+        print('不处理本地图片: ', url)
+        return ""
+    if not img_url_check(url):
+        print('用户自定义图片url检查失败：',url)
+        return ""
+
+    img_data = requests.get(url).content # 获取到的图片url文件data
+    filename = url[url.rfind('/')+1:] # 图片源文件名
+    # 如果filename中不包含.认为其没有文件后缀，给他添加上一个
+    if '.' not in filename:
+        filename += DEFUALT_IMG_TYPE
+
+    print('图片文件名：',filename)
+
+    # 保存本地，用图片文件名命名
+    with open(os.path.join(TARGET_DIR, filename), 'w+') as f:
         f.buffer.write(img_data)
+    return filename
 ```
 
 ## 欢迎与我交流
--- spider.py
+++ spider.py
@@ -8,19 +8,41 @@ from zoneinfo import ZoneInfo # python 3.9+
 import time
 import traceback
 
-TARGET_DIR ='./files/img/' 
+###########################################################################################
+
+# 以下你可以根据自己的需要进行修改
+IS_REPLACE_LINK = True
+"""是否修改md文件中的链接？"""
+DEFUALT_IMG_TYPE = '.png'
+"""
+给无后缀的图片url新增文件扩展名
+（建议测试一下到底是什么类型的图片，一般都是png）
+"""
+
+def img_url_check(img_url:str):
+    """
+    你可以修改这个函数，比如只下载某一部分url中的图片。
+    返回值为True代表需要下载当前图片
+    """
+    return True
+
+
+###########################################################################################
+
+TARGET_DIR ='./files/img/'
 """存放下载好的本地图片的目录"""
 MD_FILES_DIR = './files'
 """存放md文件的文件夹 """
+err_img = {}
+"""保存错误的图片dict"""
 
-
-def open_file(path):
+def open_json_file(path):
     """打开json文件"""
     with open(path, 'r', encoding='utf-8') as f:
         tmp = json.load(f)
     return tmp
 
-def write_file(path: str, value):
+def write_json_file(path: str, value):
     """写入json文件"""
     with open(path, 'w', encoding='utf-8') as fw2:
         json.dump(value, fw2, indent=2, sort_keys=True, ensure_ascii=False)
@@ -30,15 +52,13 @@ def get_time(format_str='%y-%m-%d-%H%M%S'):
     a = datetime.now(ZoneInfo('Asia/Shanghai'))  # 返回北京时间
     return a.strftime(format_str)
 
-err_img = {}
-"""保存错误的图片dict"""
-ERR_FILE_PATH = f"{get_time()}-err.json"
-"""保存错误图片的路径"""
-
 def write_err_file():
     """写入日志文件"""
+    ERR_FILE_PATH = f"{get_time()}-err.json"
+    """保存错误图片的路径"""
+
     print(f"\n[file] err_img\n{err_img}") # 保存之前打印，避免保存失败
-    write_file(ERR_FILE_PATH,err_img) # 写入文件
+    write_json_file(ERR_FILE_PATH,err_img) # 写入文件
     print(f"[file] write file | {ERR_FILE_PATH}") # 写入成功
 
 
@@ -72,62 +92,98 @@ def get_pics_list(md_content):
     return pics_list
 
 
-def download_pics(url):
+def download_pics(url:str):
+    """
+    下载图片并保存到本地
+
+    Returns:
+    - 空字符串：非网络图片，处理失败
+    - 非空字符串：图片本地文件名
+    """
     if 'http' not in url:
         print('不处理本地图片: ', url)
-        return
-    img_data = requests.get(url).content # 获取到的图片url
+        return ""
+    if not img_url_check(url):
+        print('用户自定义图片url检查失败：',url)
+        return ""
+
+    img_data = requests.get(url).content # 获取到的图片url文件data
     filename = url[url.rfind('/')+1:] # 图片源文件名
+    # 如果filename中不包含.认为其没有文件后缀，给他添加上一个
+    if '.' not in filename:
+        filename += DEFUALT_IMG_TYPE
+
     print('图片文件名：',filename)
-    # 如果目标文件目录不存在，创建文件目录
-    if not os.path.exists(TARGET_DIR):
-        os.mkdir(TARGET_DIR)
-    
+
     # 保存本地，用图片文件名命名
     # 原作者用的是uuid随机命名，其实这样更好，但是：前提得替换md文件中的图片链接
     # 可原作者并没有写这部分的代码，本人对本项目用到的模块并不了解，所以没有进行此功能开发
     with open(os.path.join(TARGET_DIR, filename), 'w+') as f:
         f.buffer.write(img_data)
-    
+    return filename
+
 
 
 if __name__ == '__main__':
-    print("开始处理\n")
-    # 获取MD_FILES_DIR路径下的所有md文件列表
-    files_list = get_files_list(os.path.abspath(os.path.join('.', MD_FILES_DIR)))
+    try:
+        print("开始处理\n")
+        # 获取MD_FILES_DIR路径下的所有文件列表
+        files_list = get_files_list(os.path.abspath(os.path.join('.', MD_FILES_DIR)))
+        # 如果目标文件目录不存在，创建文件目录
+        if not os.path.exists(TARGET_DIR):
+            os.mkdir(TARGET_DIR)
 
-    for file in files_list:
-        print(f'正在处理：{file}')
-        # utf-8会有编码报错。所以使用如下编码
-        with open(file, encoding='ISO-8859-1') as f:
-            md_content = f.read()
-        # 获取图片列表
-        pics_list = get_pics_list(md_content)
-        print(f'发现图片 {len(pics_list)} 张')
+        md_content = ""
+        for file in files_list:
+            if '.md' not in file:
+                print(f"不处理非md文件: {file}")
+                continue
 
-        md_file_name = "" # 初始化为空
-        for index, pic in enumerate(pics_list):
-            try:
-                print(f'正在下载第 {index + 1} 张图片...')
-                md_file_name = os.path.basename(file) # 当前处理的md文件的名字
-                # 处理图片
-                download_pics(pic)
-                time.sleep(0.3) # 避免下载超速
-            except KeyboardInterrupt:
-                write_err_file() # 写入日志文件
-                os.abort() # 避免无法ctrl+c
-            except:
-                print(traceback.format_exc()) # 打印错误
-                # 判断err_img，如果文件名不在里面，则新增键值
-                if md_file_name not in err_img:
-                    err_img[md_file_name]=[]
-                # 添加err图片
-                err_img[md_file_name].append(pic)
-                print("图片获取错误：",pic)
-                time.sleep(1)
-        # 处理完毕单个文件
-        print(f'处理完毕：{file}\n')
-    
-    # 结束后保存err
-    print('\n全部处理完成。')
-    write_err_file()
+            print(f'正在处理md文件：{file}')
+            # utf-8会有编码报错。所以使用如下编码读取md文件
+            with open(file, encoding='ISO-8859-1') as f:
+                md_content = f.read()
+            # 获取图片列表
+            pics_list = get_pics_list(md_content)
+            print(f'发现图片 {len(pics_list)} 张')
+
+            md_file_name = "" # 初始化为空
+            for index, pic in enumerate(pics_list):
+                try:
+                    print(f'正在下载第 {index + 1} 张图片...\n图片链接：', pic)
+                    md_file_name = os.path.basename(file) # 当前处理的md文件的名字
+                    # 处理图片
+                    new_img_file_name = download_pics(pic)
+                    # 修改md文件内容
+                    if IS_REPLACE_LINK and new_img_file_name != "":
+                        new_img_file_path = os.path.abspath(os.path.join('.',TARGET_DIR,new_img_file_name))
+                        new_img_file_path = new_img_file_path.replace('\\','/') # 路径转linux风格
+                        md_content = md_content.replace(pic,new_img_file_path)
+
+                    time.sleep(0.3) # 避免下载超速
+                except KeyboardInterrupt:
+                    write_err_file() # 写入日志文件
+                    os.abort() # 避免无法ctrl+c
+                except Exception as result:
+                    print(traceback.format_exc()) # 打印错误
+                    # 判断err_img，如果文件名不在里面，则新增键值
+                    if md_file_name not in err_img:
+                        err_img[md_file_name]=[]
+                    # 添加err图片
+                    err_img[md_file_name].append(pic)
+                    print("图片获取错误：",pic)
+                    time.sleep(1)
+
+            # 修改后的md写入文件
+            with open(file, encoding='ISO-8859-1',mode='w') as f:
+                f.write(md_content)
+            # 处理完毕单个文件
+            print(f'处理完毕md文件：{file}\n')
+
+        # 结束后保存err
+        print('\n全部处理完成。')
+        write_err_file()
+    except Exception as result:
+        print(traceback.format_exc())
+        print("\n错误图片输出：\n",err_img)
+        os.abort()
